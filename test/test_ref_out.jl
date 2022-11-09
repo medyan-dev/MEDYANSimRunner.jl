076be729d521da95dc17e21a00244b2d726805e5
nhz2 <nhz2@cornell.edu>
2022-11-09 10:36:50 -0500

    test restarting

--- test/test_ref_out.jl
+++ test/test_ref_out.jl
@@ -16,3 +16,13 @@ using MEDYANSimRunner
         @test false
     end
 end
+@testset "good partial example" begin
+    mkpath("examples/good partial/output")
+    cp("examples/good partial/output partial", "examples/good partial/output"; force=true)
+    MEDYANSimRunner.run("examples/good/input/", "examples/good partial/output/", 1)
+    out_diff = sprint(MEDYANSimRunner.diff, "examples/good/output-ref/out1", "examples/good partial/output/out1")
+    if !isempty(out_diff)
+        println(out_diff)
+        @test false
+    end
+end
