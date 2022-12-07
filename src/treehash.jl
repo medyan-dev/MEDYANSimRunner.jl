873d797e68e042f38b413126210c9c82d9a5d40d
nhz2 <nhz2@cornell.edu>
2022-12-07 08:39:01 -0500

    don't ignore .zarr files

--- src/treehash.jl
+++ src/treehash.jl
@@ -6,7 +6,7 @@ Return a vector of bytes representing the hash of a directory.
 
 This is just used as a weak kind of checksum, not for cryptographic purposes.
 
-Any sub-directories or files with names beginning with a "." will be ignored.
+Any sub-directories or files with names equal to ".DS_Store" will be ignored.
 Empty directories will NOT be ignored.
 Any link will be ignored.
 File permission and other metadata will be ignored.
@@ -19,7 +19,7 @@ function my_tree_hash(path::AbstractString)::Vector{UInt8}
     elseif isdir(path)
         names = filter(readdir(path)) do name
             namepath = joinpath(path,name)
-            !islink(namepath) && !startswith(name,'.')
+            !islink(namepath) && name != ".DS_Store"
         end
         content_hashs = UInt8[]
         for name in names
