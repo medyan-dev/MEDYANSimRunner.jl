042ad6a33d29a992fbc9eeed973f8a324287d8b1
nhz2 <nhz2@cornell.edu>
2025-03-06 00:12:18 -0500

    update example

--- test/example/main.jl
+++ test/example/main.jl
@@ -48,7 +48,7 @@ function loop(step::Int, state; output::ZGroup, kwargs...)
     a = sum(state) + rand(0:1)
     state[1] = state[2]
     state[2] = a
-    attrs(output)["sum"] = sum(state)
+    attrs(output)["sum"] = sum(state) + step
     state
 end
 
